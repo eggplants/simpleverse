795d2f3844425c3b9bc5f3f8103dba949d0fccf9
eggplants <w10776e8w@yahoo.co.jp>
2021-09-21 15:14:26 +0900

    add: cmd

--- simpleverse/get_image.py
+++ simpleverse/get_image.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import requests
 
@@ -15,5 +15,27 @@ class GetImageInfo(BaseVerseRequests):
     def get_image(self, image_id: str) -> ImageInfo:
         self.validate_parameter(image_id, 36, 36, "image_id")
         res = requests.get(url=self.get_endpoint("/image/" + image_id))
+        self.validate_response(res)
+        return res.json()
+
+    def get_image_OData(
+        self,
+        filter_: Optional[str] = None,
+        order_by: Optional[str] = None,
+        limit: Optional[int] = None,
+        skip: Optional[int] = None,
+    ) -> List[ImageInfo]:
+        params = {}
+        if filter_ is not None:
+            params["$filter"] = filter_
+        if order_by is not None:
+            params["$order_by"] = order_by
+        if limit is not None:
+            assert limit > 0, "limit should be positive int"
+            params["$limit"] = str(limit)
+        if skip is not None:
+            assert skip > 0, "skip should be positive int"
+            params["$skip"] = str(skip)
+        res = requests.get(self.URL + "/image/all", params=params)
         self.validate_response(res)
         return res.json()
--- simpleverse/main.py
+++ simpleverse/main.py
@@ -1,15 +1,28 @@
 import argparse
+from base64 import b64encode
+from imghdr import what
 from json import dumps
 from typing import Any
 
-from .create_user import CreateUser
-from .get_post import GetPostInfo
-from .get_user import GetUserInfo
-from .submit_post import SubmitPost
+from simpleverse import (
+    CreateLike,
+    CreateUser,
+    GetImageInfo,
+    GetLikeInfo,
+    GetPostInfo,
+    GetUserInfo,
+    SubmitImage,
+    SubmitPost,
+)
 
 
 def pprint_json(json_obj: Any) -> None:
     print(dumps(json_obj, sort_keys=True, indent=4, ensure_ascii=False))
+
+
+def func_create_like(ns: argparse.Namespace) -> None:
+    c = CreateLike()
+    print(c.create_like(ns.post_id, ns.like_count))
 
 
 def func_create_user(ns: argparse.Namespace) -> None:
@@ -20,6 +33,22 @@ def func_create_user(ns: argparse.Namespace) -> None:
 def func_update_user(ns: argparse.Namespace) -> None:
     c = CreateUser()
     print(c.update_user(ns.name, ns.description))
+
+
+def func_get_like(ns: argparse.Namespace) -> None:
+    g = GetLikeInfo()
+    if ns.post_id is None:
+        pprint_json(g.get_like_all())
+    else:
+        pprint_json(g.get_like(ns.post_id))
+
+
+def func_get_image(ns: argparse.Namespace) -> None:
+    g = GetImageInfo()
+    if ns.image_id is None:
+        pprint_json(g.get_image_all())
+    else:
+        pprint_json(g.get_image(ns.image_id))
 
 
 def func_get_post(ns: argparse.Namespace) -> None:
@@ -43,6 +72,16 @@ def func_submit_post(ns: argparse.Namespace) -> None:
     print(s.submit_post(ns.text, rep_user_id=ns.user_id, rep_post_id=ns.post_id))
 
 
+def func_submit_image(ns: argparse.Namespace) -> None:
+    s = SubmitImage()
+    body = repr(b64encode(open(ns.image, "rb").read()))
+    img_type = what(ns.image)
+    if img_type is None:
+        raise IOError("Is not given file a sort of an image?")
+    d = "data:image/" + img_type + ";base64," + body
+    print(s.submit_image(d, ns.post_id))
+
+
 def parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="simv",
@@ -53,6 +92,25 @@ def parse() -> argparse.Namespace:
     parser.set_defaults(func=lambda _: parser.print_usage())
 
     subparsers = parser.add_subparsers()
+
+    create_like_parser = subparsers.add_parser("create_like", aliases=["cl"], help="")
+    create_like_parser.add_argument(
+        "-p",
+        "--post_id",
+        metavar="POSTID",
+        type=str,
+        help="id of a specific post",
+        required=True,
+    )
+    create_like_parser.add_argument(
+        "-n",
+        "--like_count",
+        metavar="POSITIVE",
+        type=int,
+        help="count of like you want to submit",
+        required=True,
+    )
+    create_like_parser.set_defaults(func=func_create_like)
 
     create_user_parser = subparsers.add_parser("create_user", aliases=["cu"], help="")
     create_user_parser.add_argument(
@@ -81,6 +139,18 @@ def parse() -> argparse.Namespace:
         help="self introduce",
     )
     update_user_parser.set_defaults(func=func_update_user)
+
+    get_like_parser = subparsers.add_parser("get_like", aliases=["gl"], help="")
+    get_like_parser.add_argument(
+        "-p", "--post_id", metavar="POSTID", type=str, help="id of a specific post"
+    )
+    get_like_parser.set_defaults(func=func_get_post)
+
+    get_image_parser = subparsers.add_parser("get_image", aliases=["gi"], help="")
+    get_image_parser.add_argument(
+        "-i", "--image_id", metavar="IMAGEID", type=str, help="id of a specific image"
+    )
+    get_image_parser.set_defaults(func=func_get_image)
 
     get_post_parser = subparsers.add_parser("get_post", aliases=["gp"], help="")
     get_post_parser.add_argument(
@@ -111,6 +181,19 @@ def parse() -> argparse.Namespace:
         help="id of a specific post you reply to",
     )
     submit_post_parser.set_defaults(func=func_submit_post)
+
+    submit_image_parser = subparsers.add_parser("submit_image", aliases=["si"], help="")
+    submit_image_parser.add_argument(
+        "file", metavar="IMAGE", type=str, help="post image"
+    )
+    submit_image_parser.add_argument(
+        "-p",
+        "--post_id",
+        metavar="POSTID",
+        type=str,
+        help="id of a specific post you reply to",
+    )
+    submit_image_parser.set_defaults(func=func_submit_image)
 
     return parser.parse_args()
 
